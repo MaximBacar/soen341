885d62f62b9ac02f1f3f47369ad8bdfd3a03c1e8
Maxim Bacar <maximbacar@hotmail.ca>
2023-02-09 22:30:14 -0500

    Add postings and applications to database

--- SQL/wejob.sql
+++ SQL/wejob.sql
@@ -19,8 +19,24 @@ CREATE TABLE users(
     `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
 
-)
+);
 
+CREATE TABLE postings(
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `post_time` DATETIME NOT NULL,
+    `description` TEXT NOT NULL,
+    `employer_id` INT NOT NULL,
+    FOREIGN KEY (`employer_id`) REFERENCES employers(`id`) 
+);
+
+CREATE TABLE applications(
+    `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    `user_id` INT NOT NULL,
+    `post_id` INT NOT NULL,
+    FOREIGN KEY (`user_id`) REFERENCES users(`id`),
+    FOREIGN KEY (`post_id`) REFERENCES postings(`id`)
+);
 
 CREATE TABLE messages(
     `id` INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
@@ -31,5 +47,5 @@ CREATE TABLE messages(
     FOREIGN KEY (`receive_id`) REFERENCES users(`id`),
 
 
-)
+);
 
