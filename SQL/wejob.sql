f38b1fab8304da7d1635b4afa466b5808fe888df
Maxim Bacar <maximbacar@hotmail.ca>
2023-02-19 13:32:27 -0500

    Modify database

--- SQL/wejob.sql
+++ SQL/wejob.sql
@@ -8,7 +8,7 @@ CREATE TABLE employers(
     `name` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
-    `description` text,
+    `description` TEXT,
     `logo` VARCHAR(255)
 );
 
@@ -17,8 +17,7 @@ CREATE TABLE users(
     `first_name` VARCHAR(255) NOT NULL,
     `last_name` VARCHAR(255) NOT NULL,
     `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-
+    `password` VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE postings(
