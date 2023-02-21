1f04adfce82c92a327f6d45f68afddaaa7feeec8
mzmddi <110358757+mzmddi@users.noreply.github.com>
2023-02-21 14:23:50 -0500

    implemented foundation for Links, not yet added

--- we-job/src/pages/homepage.js
+++ we-job/src/pages/homepage.js
@@ -1,13 +1,16 @@
 import Navbar from "../Components/navbar";
 import Footer from "../Components/footer";
 import "./homepage.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 
 function Homepage() {
   return (
-    <div id="home">
-      <Navbar />
-      <Footer />
-    </div>
+
+      <div id="home">
+        <Navbar />
+        <Footer />
+      </div>
+   
   );
 }
 export default Homepage;
