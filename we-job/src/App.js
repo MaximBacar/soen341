551f8fd8ca98adb085dd0c5b0977d821c80f7d48
mzmddi <110358757+mzmddi@users.noreply.github.com>
2023-02-26 14:22:26 -0500

    Changes index.js to include BrowserRouter

--- we-job/src/App.js
+++ we-job/src/App.js
@@ -3,17 +3,17 @@ import React from "react";
 import "./App.css";
 import FirstPage from "./pages/firstPage";
 import Login from "./pages/login";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Router, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <Router> 
 
-        {<Homepage />}
-        {/*<FirstPage />*/}
-        {/*<Login />*/}
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/Login" element={<Login/>} />
+        
+      </Routes>
 
-    </Router>
   );
 }
 
