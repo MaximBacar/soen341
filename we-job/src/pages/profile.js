d2725e0ea8726dd4971c7ad7c17359386ad2c94e
mzmddi <110358757+mzmddi@users.noreply.github.com>
2023-02-28 16:12:21 -0500

    created profile "no user" message

--- we-job/src/pages/profile.js
+++ we-job/src/pages/profile.js
@@ -1,23 +1,53 @@
 import { Link } from "react-router-dom";
 import logo from "../Components/photo/wejob-low-resolution-logo-color-on-transparent-background.png";
+import { useState } from "react";
+import "./profile.css";
+import Footer from "../Components/footer";
 
-function Profile() {
+function Profile({ state }) {
+
+  const [noUser, setnoUser] = useState(true); 
+  const[employer, setEmployer] = useState(false); 
+  const[student, setStudent] =useState(false); 
+
+  switch(state) {
+    case "student": setStudent(true); setEmployer(false); setnoUser(false); 
+    case "employer": setEmployer(true); setStudent(false); setnoUser(false); 
+    case "noUser": setnoUser(true); setEmployer(false); setStudent(false);
+  }
+
+
   //we have to create a system where the profile will show the available profile if the user is signed in or not
   //the profile page will have three variation: employer, student, not signed in
   //the not signed in will be the easier of the three and will consist of only a message saying to sign in
 
-  const profilestate = ""; 
-
   return (
     <div class="profilepage">
-      <header class="header">
+      <header className="header">
         <div id="logo">
-          <Link to="/" exact>
+          <Link to="/">
             <img src={logo} />
           </Link>
         </div>
+        <h1>Profile</h1>
       </header>
+      <div class="body">
+        {noUser && (<div class="noUser">
+          <div class="message1">
+            Oops...Looks like you did not sign up yet! 
+          </div>
+          <div class="message2">
+            Log into your account now or create a new account! 
+          </div>
+          <div class="links">
+            <Link to="/login"class="login">Login</Link>
+            <Link to="/signup"class="signup">Sign up</Link>
+          </div>
+          
+        </div>) }
 
+      </div>
+      <Footer />
     </div>
   );
 }
